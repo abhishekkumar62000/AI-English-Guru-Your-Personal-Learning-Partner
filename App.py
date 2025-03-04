2ee3d2c0a0164b06de06662acd686cc1be646d01
Abhishek Kumar <159156426+abhishekkumar62000@users.noreply.github.com>
2025-03-04 13:20:00 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,11 +1,12 @@
 import spacy
+import subprocess
 
 try:
-    nlp = spacy.load("en_core_web_sm")
+    spacy.load("en_core_web_sm")
 except OSError:
-    import os
-    os.system("python -m spacy download en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
+    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
+    spacy.load("en_core_web_sm")
+
 
 
 # Fix event loop error
